ec5a4c4190c8b65a24b90f31f6943a562f7cae7b
rityririi <rityririi@gmail.com>
2026-02-11 11:53:04 +0900

    Update dbconn.php

--- common/dbconn.php
+++ common/dbconn.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-$conn = mysqli_connect("localhost", "root", "P@ssw0rd", "hotel");
+$conn = mysqli_connect("192.168.70.50", "root", "P@ssw0rd", "hotel");
 
 if (!$conn) {
     die("연결 실패했습니다: " . mysqli_connect_error());
